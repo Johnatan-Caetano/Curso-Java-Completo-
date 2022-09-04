e147a5817f67f78b7968c4541afe2ce86c8d2152
Johnatan Caetano <104772832+Johnatan-Caetano@users.noreply.github.com>
2022-09-04 15:44:42 -0300

    Exercícios Java Orientado a Objetos

---
+++ curso_programacao/src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double xA, xB, xC, yA, yB, yC;
+		
+		System.out.println("Digite as medidas do triângulo X: ");
+		xA = sc.nextDouble();
+		xB = sc.nextDouble();
+		xC = sc.nextDouble();
+		
+		System.out.println("Digite as medidas do triângulo Y: ");
+		yA = sc.nextDouble();
+		yB = sc.nextDouble();
+		yC = sc.nextDouble();
+		
+		double p = (xA + xB + xC)/2.0;
+		double areaX = Math.sqrt(p*(p-xA)*(p-xB)*(p-xC));
+		System.out.printf("A área do triângulo x é: %.4f%n", areaX);
+		
+		p = (yA + yB + yC)/2.0;
+		double areaY = Math.sqrt(p*(p-yA)*(p-yB)*(p-yC));
+		System.out.printf("A área do triângulo y é: %.4f%n", areaY);
+				
+		if (areaX > areaY) {
+			System.out.println("A maior área é a do triângulo X");
+		}
+		
+		else {
+			System.out.println("A maior área é a do triângulo Y");
+		}
+				
+				
+				
+		sc.close();
+	}
+
+}
